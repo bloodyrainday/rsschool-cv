0223f708208bc555427dd00856838d613d5d81e1
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-08-10 22:27:36 +0300

    feat: add an animation to the burger-menu

--- library/index.js
+++ library/index.js
@@ -1,1 +1,7 @@
 console.log("100/100 Вёрстка валидная \n 10/10 Вёрстка семантическая \n 16/16 Вёрстка соответствует макету \n 54/54 Общие требования к верстке 20/20")
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('burger').addEventListener('click', function() {
+        document.querySelector('.header').classList.toggle('open')
+    })
+})
