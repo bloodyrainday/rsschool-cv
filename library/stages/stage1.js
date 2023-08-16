d71b79c717ef73b516ba4666e1501daa12498d58
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-08-16 18:20:19 +0300

    feat: add function for switch buttons in an about section

--- library/stages/stage1.js
+++ library/stages/stage1.js
@@ -2,23 +2,36 @@ let offset = 0;
 
 const aboutSlider = document.querySelector('.about-slider')
 
-document.querySelector('.about-switcher-button-wrapper.left').addEventListener('click', (leftButton) => {
-    offset += 475;
+document.querySelector('.about-switcher-button-wrapper.left').addEventListener('click', () => {
+    document.querySelector('.about-switcher-button-left').classList.add('about-buttons-color');
+    offset += 950;
     if (offset > 0) {
-        offset = -950;
-    }
-    aboutSlider.style.left = offset + 'px'
-})
-
-/*document.querySelector('.about-switcher-button-left').addEventListener('click', () => {
-    offset += 450;
-    aboutSlider.style.left = -offset + 'px'
-})*/
-
-document.querySelector('.about-switcher-button-wrapper.right').addEventListener('click', () => {
-    offset -= 475;
-    if (offset < -950) {
         offset = 0;
     }
-    aboutSlider.style.left = offset + 'px'
+    aboutSlider.style.left = offset + 'px';
+    document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color');
+    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');
+    
+})
+
+document.querySelector('.about-switcher-button-wrapper.center').addEventListener('click', () => {
+    document.querySelector('.about-switcher-button-center').classList.add('about-buttons-color');
+    offset -= 475;
+    if (offset < -475) {
+        offset = -475;
+    }
+    aboutSlider.style.left = offset + 'px';
+    document.querySelector('.about-switcher-button-left').classList.remove('about-buttons-color');
+    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');
+})
+
+document.querySelector('.about-switcher-button-wrapper.right').addEventListener('click', () => {
+    document.querySelector('.about-switcher-button-right').classList.add('about-buttons-color');
+    offset -= 950;
+    if (offset < -950) {
+        offset = -950;
+    }
+    aboutSlider.style.left = offset + 'px';
+    document.querySelector('.about-switcher-button-left').classList.remove('about-buttons-color');
+    document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color')
 })
