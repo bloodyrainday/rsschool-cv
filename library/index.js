58b51625eb30ad8104d0a214a13a5e603f70825d
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-08-20 15:29:51 +0300

    feat: burger-menu close itself after clicking on the background

--- library/index.js
+++ library/index.js
@@ -1,7 +1,20 @@
 console.log("Вёрстка соответствует макету. Ширина экрана 768px 26/26 \n Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется 12/12 \n На ширине экрана 768рх реализовано адаптивное меню 12/12  \n")
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('burger').addEventListener('click', function() {
-        document.querySelector('.header').classList.toggle('open')
-    })
-})
+const body = document.querySelector('.body-wrapper');
+const header = document.querySelector('.header');
+const nav = document.querySelector('nav');
+
+function openMenu(event) {  
+    document.querySelector('.header').classList.toggle('open')
+}
+
+
+document.getElementById('burger').addEventListener('click', openMenu)
+
+document.addEventListener('click', event => {
+    console.log(event.target)
+    const click = event.composedPath().includes(burger)
+        if (!click) {
+            document.querySelector('.header').classList.remove('open')
+        }
+      });
--- library/stages/stage1.js
+++ library/stages/stage1.js
@@ -1,37 +1,92 @@
 let offset = 0;
+const images = Array.from(document.querySelectorAll('.about-slider-item'));
+const sliderLine = document.querySelector('.about-slider');
+let count = 0;
+let width;
 
-const aboutSlider = document.querySelector('.about-slider')
+function init() {
+    width = document.querySelector('.about-slider-wrapper').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+        images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    })
+}
+
+window.addEventListener('resize', init);
+init()
+
+/*let minWidthScreen1011px = window.matchMedia('(min-width: 1011px)');
+let maxWidthScreen768px = window.matchMedia('(max-width: 768px)');*/
+console.log(Array.from(document.querySelectorAll('.about-slider-item')))
+
+function rollSlider() {
+    sliderLine.style.left = '-' + count * 475 + 'px';
+}
+
 
 document.querySelector('.about-switcher-button-wrapper.left').addEventListener('click', () => {
-    document.querySelector('.about-switcher-button-left').classList.add('about-buttons-color');
-    offset += 950;
-    if (offset > 0) {
-        offset = 0;
+    count--;
+    if (count <= images.length ) {
+        count = 0
     }
-    aboutSlider.style.left = offset + 'px';
+    rollSlider()
+    /*document.querySelector('.about-switcher-button-left').classList.add('about-buttons-color');
+    if (minWidthScreen1011px.matches) {
+            offset += 950;
+        if (offset > 0) {
+            offset = 0;
+        }
+    }
+    
+
+    sliderLine.style.left = offset + 'px';
     document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color');
-    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');
+    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');*/
     
 })
 
-document.querySelector('.about-switcher-button-wrapper.center').addEventListener('click', () => {
-    document.querySelector('.about-switcher-button-center').classList.add('about-buttons-color');
-    offset -= 475;
-    if (offset < -475) {
-        offset = -475;
+    document.querySelector('.about-switcher-button-wrapper.center').addEventListener('click', () => {
+        count++;
+        if (count >= images.length ) {
+            count = 1
+        }
+        rollSlider()
+    /*document.querySelector('.about-switcher-button-center').classList.add('about-buttons-color');
+
+    if (minWidthScreen1011px.matches) {
+        offset -= 475;
+        if (offset < -475) {
+            offset = -475;
+        }
+    } else {
+
     }
-    aboutSlider.style.left = offset + 'px';
+
+    sliderLine.style.left = offset + 'px';
     document.querySelector('.about-switcher-button-left').classList.remove('about-buttons-color');
-    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');
+    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');*/
 })
 
-document.querySelector('.about-switcher-button-wrapper.right').addEventListener('click', () => {
-    document.querySelector('.about-switcher-button-right').classList.add('about-buttons-color');
-    offset -= 950;
-    if (offset < -950) {
-        offset = -950;
+    document.querySelector('.about-switcher-button-wrapper.right').addEventListener('click', () => {
+        count++;
+    if (count >= images.length ) {
+        count = 2
     }
-    aboutSlider.style.left = offset + 'px';
+    rollSlider()
+
+    /*document.querySelector('.about-switcher-button-right').classList.add('about-buttons-color');
+
+    if (minWidthScreen1011px.matches) {
+        offset -= 950;
+        if (offset < -950) {
+            offset = -950;
+        }
+    } 
+
+
+    sliderLine.style.left = offset + 'px';
     document.querySelector('.about-switcher-button-left').classList.remove('about-buttons-color');
-    document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color')
+    document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color')*/
 })
+
