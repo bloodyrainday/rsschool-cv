0f00898aec9b601289a8e529cc70927d9864b102
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-08-21 23:12:39 +0300

    feat: stage 2 is done

--- library/stages/stage2.js
+++ library/stages/stage2.js
@@ -12,22 +12,23 @@ const inputLastName = document.getElementById('input-last-name');
 const inputEmail = document.getElementById('input-email');
 const inputPassword = document.getElementById('input-password');
 const signUpButtonRegister = document.querySelector('.popup-register-content-button');
+const checkCard = document.querySelector('.digital_library_card-form-btn');
 
 
 /*open menu of profile icon*/
 
 function clickIconProfile(event) {
-    iconsWrapper.classList.toggle('active');
+    popUpLogin.classList.toggle('active');
 }
 
-headerIconProfile.addEventListener('click', clickIconProfile);
+iconProfile.addEventListener('click', clickIconProfile);
 
 
 /*open register menu*/
 
 registerClick.addEventListener('click', (event) => {
     popupRegister.classList.toggle('hidden');
-    iconsWrapper.classList.toggle('active')
+    popUpLogin.classList.toggle('active')
 })
 
 popupRegister.addEventListener('click', (event) => {
@@ -64,15 +65,42 @@ signUpButtonRegister.addEventListener('click', () => {
         firstLetterOfName = inputFirstName.value[0].toUpperCase();
         firstLetterofLastname = inputLastName.value[0].toUpperCase();
 
-        iconProfile.innerHTML = firstLetterOfName + firstLetterofLastname
+        iconProfile.textContent = firstLetterOfName + firstLetterofLastname;
+
+        iconProfile.classList.add('activated')
+
+        /* card number */
+
+        let cardNumber = [];
+        let randomNumbers;
+        while (cardNumber.length < 9) {
+            randomNumbers = Math.floor(Math.random() * 16).toString(16);
+            cardNumber.push(randomNumbers)
+        }
+        localStorage.setItem('user-card-number', cardNumber.join('').toUpperCase())
+        /*console.log('Here is your card number ' + cardNumber.join('').toUpperCase())*/
+    } else {
+        console.log('fill in the gaps')
     }
     
     console.log(firstLetterOfName, firstLetterofLastname)
-    
-        console.log('fill in the gaps')
-    
-
-    
 })
 
-console.log(localStorage)
+/* check-the-card button*/
+
+checkCard.addEventListener('click', () => {
+    if ((document.getElementById('input-readers-name').value === localStorage.getItem('user-first-name')) && (document.getElementById('input-card-number').value === localStorage.getItem('user-card-number'))) {
+        checkCard.style.display = 'none';
+        document.querySelector('.users-info').style.display = 'flex'
+        setTimeout(() => {
+            checkCard.style.display = 'block';
+            document.querySelector('.users-info').style.display = 'none';
+            document.getElementById('input-readers-name').value = '';
+            document.getElementById('input-card-number').value = '';
+        }, 10000)
+    } else {
+        console.log('this name or card number does not exist!')
+    }
+    
+    
+})
