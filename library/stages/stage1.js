fb03b7664954fe7df4c38952f41ae1fc780499bc
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-08-26 18:21:23 +0300

    feat: add an adaptive slider in an about section

--- library/stages/stage1.js
+++ library/stages/stage1.js
@@ -1,12 +1,21 @@
 let offset = 0;
-const images = Array.from(document.querySelectorAll('.about-slider-item'));
+const images = document.querySelectorAll('.about-slider-item');
 const sliderLine = document.querySelector('.about-slider');
 let count = 0;
 let width;
 
+
 function init() {
     width = document.querySelector('.about-slider-wrapper').offsetWidth;
-    sliderLine.style.width = width * images.length + 'px';
+    console.log(width);
+    if (width >= 1400) {
+        sliderLine.style.width = width + 'px';
+    } else if ((width < 1400) && (width >= 970)) {
+        sliderLine.style.width = width * (images.length - 2.5) + 'px';
+    } else if (width < 970) {
+        sliderLine.style.width = width * (images.length) + 'px';
+    }
+    
         images.forEach(item => {
         item.style.width = width + 'px';
         item.style.height = 'auto';
@@ -16,66 +25,64 @@ function init() {
 window.addEventListener('resize', init);
 init()
 
-/*let minWidthScreen1011px = window.matchMedia('(min-width: 1011px)');
-let maxWidthScreen768px = window.matchMedia('(max-width: 768px)');*/
+let minWidthScreen1011px = window.matchMedia('(min-width: 1011px)');
+let maxWidthScreen768px = window.matchMedia('(max-width: 768px)');
 console.log(Array.from(document.querySelectorAll('.about-slider-item')))
 
-function rollSlider() {
+/*function rollSlider() {
     sliderLine.style.left = '-' + count * 475 + 'px';
-}
+}*/
 
 
 document.querySelector('.about-switcher-button-wrapper.left').addEventListener('click', () => {
-    count--;
+    /*count--;
     if (count <= images.length ) {
         count = 0
-    }
-    rollSlider()
-    /*document.querySelector('.about-switcher-button-left').classList.add('about-buttons-color');
+    }*/
+    /*rollSlider()*/
+    document.querySelector('.about-switcher-button-left').classList.add('about-buttons-color');
     if (minWidthScreen1011px.matches) {
             offset += 950;
         if (offset > 0) {
             offset = 0;
         }
-    }
+    } 
     
 
     sliderLine.style.left = offset + 'px';
     document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color');
-    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');*/
+    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');
     
 })
 
     document.querySelector('.about-switcher-button-wrapper.center').addEventListener('click', () => {
-        count++;
+        /*count++;
         if (count >= images.length ) {
             count = 1
-        }
-        rollSlider()
-    /*document.querySelector('.about-switcher-button-center').classList.add('about-buttons-color');
+        }*/
+        /*rollSlider()*/
+    document.querySelector('.about-switcher-button-center').classList.add('about-buttons-color');
 
     if (minWidthScreen1011px.matches) {
         offset -= 475;
         if (offset < -475) {
             offset = -475;
         }
-    } else {
-
-    }
+    } 
 
     sliderLine.style.left = offset + 'px';
     document.querySelector('.about-switcher-button-left').classList.remove('about-buttons-color');
-    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');*/
+    document.querySelector('.about-switcher-button-right').classList.remove('about-buttons-color');
 })
 
     document.querySelector('.about-switcher-button-wrapper.right').addEventListener('click', () => {
-        count++;
+        /*count++;
     if (count >= images.length ) {
         count = 2
-    }
-    rollSlider()
+    }*/
+    /*rollSlider()*/
 
-    /*document.querySelector('.about-switcher-button-right').classList.add('about-buttons-color');
+    document.querySelector('.about-switcher-button-right').classList.add('about-buttons-color');
 
     if (minWidthScreen1011px.matches) {
         offset -= 950;
@@ -87,6 +94,6 @@ document.querySelector('.about-switcher-button-wrapper.left').addEventListener('
 
     sliderLine.style.left = offset + 'px';
     document.querySelector('.about-switcher-button-left').classList.remove('about-buttons-color');
-    document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color')*/
+    document.querySelector('.about-switcher-button-center').classList.remove('about-buttons-color')
 })
 
