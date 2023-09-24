f9f863d09c2a77f01c14e9a57bfe2c0109957cc2
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-09-24 14:51:08 +0300

    feat: add play and rotate functions

--- js30#1.2-audio-player/script.js
+++ js30#1.2-audio-player/script.js
@@ -5,6 +5,39 @@ const prevButton = document.querySelector('.box-buttons-prev');
 const audio = document.getElementById('song');
 const progressContainer = document.querySelector('.progress-container');
 const progress = document.querySelector('.progress');
-const artist = document.querySelector('.box-name.artist');
+const songName = document.querySelector('.box-name.song');
 const cover = document.querySelector('.player-cover');
 const imgSrc = document.getElementById('img_src');
+const songs = ['badplace', 'lostinthemail', 'trying'];
+
+let songIndex = 0;
+
+function loadSong(song) {
+    songName.innerHTML = song;
+    audio.src = `audio/${song}.mp3`;
+    cover.src = `images/cover${songIndex + 1}.jpg`
+}
+
+loadSong(songs[songIndex])
+
+function playSong() {
+    player.classList.add('play');
+    cover.classList.add('active');
+    audio.play()
+}
+
+function pauseSong() {
+    player.classList.remove('play');
+    cover.classList.remove('active');
+    audio.pause()
+}
+
+playButton.addEventListener('click', () => {
+    const isPlaying = player.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
