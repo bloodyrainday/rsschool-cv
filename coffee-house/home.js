fe1bc453e97a7186d7d6177af1ff7f9e4b29d8db
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-12-09 22:41:16 +0300

    feat: add code to make burger menu work on Home page

---
+++ coffee-house/home.js
@@ -0,0 +1,18 @@
+const burgerMenuButton = document.querySelector('.nav-menu');
+const burgerMenuItems = document.querySelectorAll(".burger-menu-links-list-item");
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuCloseButton = document.querySelector('.nav-menu-burger');
+
+burgerMenuButton.addEventListener('click', (event) => {
+    burgerMenu.classList.toggle('active');
+    burgerMenuCloseButton.classList.toggle('active');
+});
+
+    
+for (let i = 0; i <= burgerMenuItems.length; i++) {
+    burgerMenuItems[i].addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        burgerMenuCloseButton.classList.toggle('active');
+    })
+}
+
