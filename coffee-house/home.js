30c4018ff68d97fef52cdcecafe98ee1e20c4877
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2023-12-10 18:52:07 +0300

    feat: add functions to make slider roll

--- coffee-house/home.js
+++ coffee-house/home.js
@@ -2,6 +2,13 @@ const burgerMenuButton = document.querySelector('.nav-menu');
 const burgerMenuItems = document.querySelectorAll(".burger-menu-links-list-item");
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerMenuCloseButton = document.querySelector('.nav-menu-burger');
+const slider = document.querySelector('.favorite_coffee-slider-center');
+const slides = document.querySelectorAll(".favorite_coffee-slider-center-slide");
+const sliderLine = document.querySelector('.favorite_coffee-slider-center-slide_line');
+let count = 0;
+let width;
+
+/* BURGER MENU START */
 
 burgerMenuButton.addEventListener('click', (event) => {
     burgerMenu.classList.toggle('active');
@@ -9,10 +16,49 @@ burgerMenuButton.addEventListener('click', (event) => {
 });
 
     
-for (let i = 0; i <= burgerMenuItems.length; i++) {
+/*for (let i = 0; i <= burgerMenuItems.length; i++) {
     burgerMenuItems[i].addEventListener('click', () => {
         burgerMenu.classList.toggle('active');
         burgerMenuCloseButton.classList.toggle('active');
     })
+}*/
+
+/* BURGER MENU END */
+
+/* SLIDER START */
+
+function init() {
+    width = slider.offsetWidth;
+    sliderLine.style.width = width * slides.length + 'px';
+    slides.forEach( item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    })
+    console.log(width)
+    rollSlider();
 }
 
+window.addEventListener('resize', init)
+
+init();
+
+document.querySelector('.favorite_coffee-slider-arrow.right').addEventListener('click', function() {
+    count++;
+    if (count >= slides.length) {
+        count = 0;
+    }
+    rollSlider();
+})
+
+document.querySelector('.favorite_coffee-slider-arrow.left').addEventListener('click', function() {
+    count--;
+    if (count < 0) {
+        count = slides.length - 1;
+    }
+    rollSlider();
+})
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
+/* SLIDER END */
