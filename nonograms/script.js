e1ecddb390db345f1bed6a983773dc86e5efeaeb
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-01-25 21:44:45 +0300

    feat: add pop-up to start the game

---
+++ nonograms/script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+const startPopupElement = document.createElement('div');
+const startPopupContentElement = document.createElement('div');
+const startTitleElement = document.createElement('h1');
+const startButtonElement = document.createElement('button');
+
+
+startPopupElement.setAttribute('class', 'start-pop-up');
+body.prepend(startPopupElement);
+
+startPopupContentElement.setAttribute('class', 'start-pop-up__content');
+startPopupElement.prepend(startPopupContentElement);
+
+startPopupContentElement.prepend(startTitleElement);
+startTitleElement.setAttribute('class', 'start-pop-up__title');
+startTitleElement.textContent = 'WELCOME TO THE NONOGRAM GAME!';
+
+startPopupContentElement.append(startButtonElement);
+startButtonElement.setAttribute('class', 'start-pop-up__button');
+startButtonElement.textContent = 'START GAME';
+
+startButtonElement.addEventListener('click', () => {
+  startPopupElement.classList.add('hidden');
+})
